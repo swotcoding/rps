58201f916f10d505f82d9a236b926943910783ec
swotcoding <swotcoding@gmail.com>
2014-04-01 18:04:51 +0000

    created rock, paper, scissors game

---
+++ rps.rb
@@ -0,0 +1,19 @@
+choices = ["scissors", "paper", "rock"]
+puts "Pick between #{choices}."
+user_choice = gets.chomp.downcase
+computer_choice = choices.sample
+
+if user_choice == "rock" && computer_choice == "scissors"
+  puts "Congratulations! You win!"
+elsif computer_choice == "rock" && user_choice == "scissors"
+  puts "Sorry! :( You lost."
+elsif
+  
+elsif
+  
+elsif
+  
+elsif
+  
+end
+  
